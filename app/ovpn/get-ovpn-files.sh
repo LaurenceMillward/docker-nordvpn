7bfa4368fcad5c1d44f14cfea3988b19947afe4d
Laurence Millward <laurencemillward@gmail.com>
2022-05-15 13:32:47 +0100

    move to wget

--- app/ovpn/get-ovpn-files.sh
+++ app/ovpn/get-ovpn-files.sh
@@ -2,26 +2,20 @@
 
 . /app/date.sh --source-only
 
-echo "$(adddate) INFO: Download and extract ovpn files"
+echo "$(adddate) INFO: Download and extract ovpn files" 
 
 #Create directory if no volume is done
 mkdir -p /app/ovpn/config
+cd /app/ovpn/config
 
 #First remove files if exists
-rm -rf ${OVPN_CONFIG_DIR}/ovpn*
+rm -rf ovpn*
 
-#Curl download ovpn files from NordVPN
-curl -s -o ${OVPN_CONFIG_DIR}/ovpn.zip ${OVPN_FILES}
+#Download the files
+wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
 
 #Unzip files
-unzip -q ${OVPN_CONFIG_DIR}/ovpn.zip -d ${OVPN_CONFIG_DIR}
-
-#Print out logging
-if [ $? -eq 0 ]; then
-    echo "$(adddate) INFO: OVPN files successfully unzipped to $OVPN_CONFIG_DIR"
-else
-    echo "$(adddate) ERROR: OVPN files unzipped failed!"
-fi
+unzip -q ovpn.zip
 
 #Remove current zip
-rm -rf ${OVPN_CONFIG_DIR}/ovpn.zip
+rm -rf ovpn.zip
