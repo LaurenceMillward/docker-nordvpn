5fe6ee1064ec6c3e5af04ebd2c65fe40dbc35841
Laurence Millward <laurencemillward@gmail.com>
2023-06-28 23:46:35 +0100

    fix: revert change

--- app/ovpn/get-ovpn-files.sh
+++ app/ovpn/get-ovpn-files.sh
@@ -12,7 +12,7 @@ cd ${OVPN_CONFIG_DIR}
 rm -rf ovpn*
 
 #Download the files
-wget https://get-nordvpn-ovpn-file.laurencemillward-uk.workers.dev/
+wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
 
 #Unzip files
 unzip -q ovpn.zip
